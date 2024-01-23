352cf440637de1de91001991b2152b63781b1582
JangWoojun <woojun0107@naver.com>
2024-01-23 22:02:04 +0900

    game_result 화면 구성

---
+++ lib/game_result.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class GameResult extends StatelessWidget {
+  const GameResult({required this.isEnd, super.key});
+  final bool isEnd;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
