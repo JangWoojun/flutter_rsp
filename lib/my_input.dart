28fa1566c92289b2ffc573af41638695dd4732e7
JangWoojun <woojun0107@naver.com>
2024-01-23 22:01:24 +0900

    my_input 화면 구성

---
+++ lib/my_input.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rsp/input_item.dart';
+
+import 'input_card.dart';
+
+class MyInput extends StatelessWidget {
+  const MyInput({required this.isEnd, super.key});
+
+  final bool isEnd;
+
+  @override
+  Widget build(BuildContext context) {
+    if (isEnd) {
+      return const Placeholder();
+    }
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: getInputs(),
+    );
+  }
+
+  List<Widget> getInputs() {
+    return InputItem.values
+        .map(
+          (imagePath) => InputCard(imagePath: imagePath.path,),
+        )
+        .toList();
+  }
+}
