55efa91d3d7be6aebc037509746ab1c9efdcde45
JangWoojun <woojun0107@naver.com>
2024-01-23 22:00:58 +0900

    main 화면 구성

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,49 @@
 import 'package:flutter/material.dart';
 
+import 'ai_input.dart';
+import 'game_result.dart';
+import 'my_input.dart';
+
+
 void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("가위 바위 보"),
+        ),
+        body: const RSP(),
+      ),
+    ),
+  );
 }
+
+class RSP extends StatefulWidget {
+  const RSP({super.key});
+
+  @override
+  State<RSP> createState() => _RSPState();
+}
+
+class _RSPState extends State<RSP> {
+  late bool isEnd;
+
+  @override
+  void initState() {
+    super.initState();
+    isEnd = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(child: AiInput(isEnd: isEnd,)),
+        Expanded(child: GameResult(isEnd: isEnd,)),
+        Expanded(child: MyInput(isEnd: isEnd,)),
+      ],
+    );
+  }
+}
+
 
